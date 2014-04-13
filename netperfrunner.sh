1cc86e26b8550ecd8246173ca2fe70e8fceebc59
Rich Brown <richb.hanover@gmail.com>
2014-04-12 23:45:04 -0400

    Editorial tweak to intro comment

--- netperfrunner.sh
+++ netperfrunner.sh
@@ -101,8 +101,7 @@ do
 done
 
 DATE=`date "+%Y-%m-%d %H:%M:%S"`
-echo "$DATE Testing $TESTHOST with $MAXSESSIONS streams down and up "
-echo "     while pinging $PINGHOST. Takes about $TESTDUR seconds."
+echo "$DATE Testing $TESTHOST with $MAXSESSIONS streams down and up while pinging $PINGHOST. Takes about $TESTDUR seconds."
 # echo "It downloads four files, and concurrently uploads four files for maximum stress."
 # echo "It also pings a well-connected host, and prints a summary of the latency results."
 # echo "This test is part of the CeroWrt project. To learn more, visit:"
